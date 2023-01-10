64a94079fdafce69daf6e704bdf9ebf61941285c
Peter Norton <pcn@toffee.tail45a02.ts.net>
2023-01-10 01:33:18 -0500

    Does a bit more, doesn't add content to a folder

--- scripts/refresh-tokens.py
+++ scripts/refresh-tokens.py
@@ -1,30 +1,51 @@
 #!/usr/bin/env python
 
-import time
+import json
 import os
+import time
 from typing import List, Dict
 
 import wmill
 import boto3
 
+from windmill_api.api.workspace import list_workspaces_as_super_admin
+from windmill_api.api.folder import list_folders, get_folder, create_folder, update_folder
+
+from windmill_api.models.create_folder_json_body import CreateFolderJsonBody
+from windmill_api.models.update_folder_json_body import UpdateFolderJsonBody
+
 # requires WM_TOKEN to be set in the environment to communicate with the
 # associated windmill instance
+# requires WM_IAM_PREFIX to be set in the environment to specify the
+# windmill roles' IAM path, e.g. '/windmill-stage/'
 
-def main(groups: list):
+def main():
+    s = boto3.session.Session(region_name="us-east-2")
+    caller_id = s.client('sts').get_caller_identity()
+    print(f"Started with caller_identity of {caller_id}")
     while True:
-        s = boto3.session.Session(region_name="us-east-2")
-        iam_c = s.client('iam')
-        sts_c = s.client('sts')
-        caller_id = sts_c.get_caller_identity()
-        for g in get_wmill_groups():
-            variable = f"g/{g}/aws"
-            wmill.set_variable(variable, get_token(g))
-            print(f"Set token for group {g}")
-            print(wmill.get_variable(variable))
+        ws_names = get_wmill_workspace_names()
+        iam_roles = get_likely_iam_roles(os.getenv("WM_IAM_PREFIX"), s)
+        for ws_name_from_iam in iam_roles.keys():
+            if ws_name_from_iam in ws_names:
+                creds = get_wspace_creds(iam_roles[ws_name_from_iam], s)
+                set_ws_folder(ws_name_from_iam, 'aws', creds)
         time.sleep(60)
 
 
-def get_likely_iam_groups(prefix: str, session: boto3.session.Session) -> Dict[str, dict]:
+def get_wspace_creds(roles: dict, s):
+    """Given a dictionary that looks like this for e.g.  wspace_name='infra',
+    roles={'ec2_ro': 'arn:aws:iam::895030038186:role/windmill-cmh-stag/w=infra/ec2_ro', [etc.]}
+    return a dictionary of role name and credentials, e.g.
+    """
+    sts = s.client('sts')
+    results = dict()
+    for rname, arn in roles.items():
+        results[rname] = sts.assume_role(RoleArn=arn, RoleSessionName=f"windmill-{rname}")
+    return results
+
+
+def get_likely_iam_roles(prefix: str, session: boto3.session.Session) -> Dict[str, dict]:
     """Role names should be prefixed by the :prefix: argument.
     The IAM role can have the filed names corresponding to the windmill workspace, and a final group.
     Since it's possible for groups and workspaces to have various charcters, the convention will be that
@@ -32,33 +53,69 @@ def get_likely_iam_groups(prefix: str, session: boto3.session.Session) -> Dict[s
 
     So if the windmill IAM role space starts with is ``windmill-stage`` and we have a workspace called ``infra`` and a group called
     ``aws-ro`` the IAM role that would be matched with that, if it exists, would be called
-    ``/windmill-stage/w=infra/g=aws-ro``
+    ``ec2-ro``
+    and it would be in the namespace:
+    ``/windmill-stage/w=infra/``
 
     """
     iam_c = session.client('iam')
     marker = None
-    all_matching_role_names = list()
+    workspace_roles = dict()
     while True:
         if marker:
             result = iam_c.list_roles(PathPrefix=prefix, Marker=marker)
         else:
             result = iam_c.list_roles(PathPrefix=prefix)
         for r in result['Roles']:
-            all_matching_role_names.append({r['RoleName']: r['Arn']})
+            pth = r['Path']
+            wspace = pth.split('/')[2][2:]  # turn /foo/w=<something>/ into <something>
+            if not workspace_roles.get(wspace):
+                workspace_roles[wspace] = dict()
+            workspace_roles[wspace][r['RoleName']] = r['Arn']
 
         if result['IsTruncated'] is False:
             break
         marker = result['Marker']
+    return workspace_roles
 
 
+def set_ws_folder(ws_name: str, temp_creds: dict):
+    folders = _list_folders(ws_name)
+    if "aws" not in folders:
+        _create_folder(ws_name)
+    _update_folder(ws_name, temp_creds)
 
-def get_aws_token(group: str, session: boto3.session.Session) -> dict[str, str]:
-    return f"token-{group}"
+def get_wmill_workspace_names() -> set:
+    """Provide a set of workspace names that really exist"""
+    workspace_names = set()
+    for wspace in list_workspaces_as_super_admin.sync(client=wmill.create_client()):
+        workspace_names.add(wspace.id)
+    return workspace_names
 
 
-def get_wmill_workspaces() -> list:
-    from windmill_api.api.workspaces import list_workspaces_as_super_admin
-    return list_workspaces_as_super_admin.sync(client=wmill.create_client(), workspace=wmill.get_workspace())
+def _list_folders(workspace):
+    """List the folders that exist in the requested workspace"""
+    res = list_folders.sync(workspace=workspace, client=wmill.create_client())
+    return res
+
+
+def _update_folder(workspace: str, content: dict):
+    """content should be a dictionary, with sub-dicts of <role
+    name>:<credentials> for each of the IAM roles that will be placed
+    in the folder.
+    """
+    request = None  # XXX I'm unsure what adds content to the folder based on the openapi
+    result = update_folder.sync_detailed(
+        workspace=workspace, name="aws", json_body=json.dumps(content), client=wmill.create_client())
+    print(f"update result is {result}")
+
+
+def _create_folder(workspace: str):
+    """If it's not there, create it"""
+    request = CreateFolderJsonBody(name="aws")
+
+    result = create_folder.sync_detailed(workspace=workspace, json_body=request, client=wmill.create_client())
+    print(f"create result is {result}")
 
 
 if __name__ == '__main__':
@@ -67,3 +124,4 @@ if __name__ == '__main__':
     except Exception as e:
         print(f"Exception bubbled up to the top: {str(e)}")
         time.sleep(2)
+        raise
